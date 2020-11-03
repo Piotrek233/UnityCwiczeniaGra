53e52884a71cfabde0524788018a8e3939544349
Piotrek233 <32534983+Piotrek233@users.noreply.github.com>
2020-11-03 19:24:51 +0100

    Pierwszy poziom
    Dodano pierwszy przykładowy poziom

---
+++ Projekt/Assets/Scripts/skok.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skok : MonoBehaviour
+{
+    public float silaSkoku = 5.0f;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // trzeba jakos to zablokowac bo narazie postac nam lata po calej mapie jak trzyma spacje xD
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * silaSkoku);
+        }
+    }
+}
