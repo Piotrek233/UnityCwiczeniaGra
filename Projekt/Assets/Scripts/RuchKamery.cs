aa50d519708a1316dd71f268c6580de6c2613c36
Piotrek233 <32534983+Piotrek233@users.noreply.github.com>
2020-11-03 15:36:33 +0100

    Kamera śledząca gracza
    Dodanie kamery podążającej za graczem

---
+++ Projekt/Assets/Scripts/RuchKamery.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuchKamery : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject sledzGracza;
+    public Vector3 pozycjaKamery;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = sledzGracza.transform.position + pozycjaKamery;
+    }
+}
